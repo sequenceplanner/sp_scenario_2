7a902d890379c86eeaeb5185c2dc53b5b549e804
endre90 <endre90@gmail.com>
2022-04-06 23:33:41 +0200

    readme and repos 2

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -493,6 +493,7 @@ def generate_launch_description():
         remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
         emulate_tty=True,
     )
+    
 
     if simple:
         nodes_to_start = [
